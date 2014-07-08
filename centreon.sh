e9dc4437e63d6fb47ca7730209ecf0ad1f9e7714
Zeysh <zeysh.zorg@gmail.com>
2014-07-08 02:35:05 +0200

    Add function for centreon template creation

--- centreon.sh
+++ centreon.sh
@@ -340,13 +340,99 @@ update-rc.d cbd defaults
 apt-get clean
 }
 
+function create_centreon_tmpl() {
 echo "
-==========================| Step 7 |=================================
+======================================================================
 
-                Install Centreon Web Interface
+                  Centreon template generation
 
-=====================================================================
+======================================================================
 "
+cat > ${DL_DIR}/${CENTREON_TMPL} << EOF
+#Centreon template
+PROCESS_CENTREON_WWW=1
+PROCESS_CENTSTORAGE=1
+PROCESS_CENTCORE=1
+PROCESS_CENTREON_PLUGINS=1
+PROCESS_CENTREON_SNMP_TRAPS=1
+
+LOG_DIR="$BASE_DIR/log"
+LOG_FILE="$LOG_DIR/install_centreon.log"
+TMPDIR="/tmp/centreon-setup"
+SNMP_ETC="/etc/snmp/"
+PEAR_MODULES_LIST="pear.lst"
+PEAR_AUTOINST=1
+
+INSTALL_DIR_CENTREON="${INSTALL_DIR}/centreon"
+CENTREON_BINDIR="${INSTALL_DIR}/centreon/bin"
+CENTREON_DATADIR="${INSTALL_DIR}/centreon/data"
+CENTREON_USER=${CENTREON_USER}
+CENTREON_GROUP=${CENTREON_GROUP}
+PLUGIN_DIR="${INSTALL_DIR}/centreon-plugins/libexec"
+CENTREON_LOG="/var/log/centreon"
+CENTREON_ETC="/etc/centreon"
+CENTREON_RUNDIR="/var/run/centreon"
+CENTREON_GENDIR="/var/cache/centreon"
+CENTSTORAGE_RRD="/var/lib/centreon"
+CENTSTORAGE_BINDIR="${INSTALL_DIR}/centreon/bin"
+CENTCORE_BINDIR="${INSTALL_DIR}/centreon/bin"
+CENTREON_VARLIB="/var/lib/centreon"
+CENTPLUGINS_TMP="/var/lib/centreon/centplugins"
+CENTPLUGINSTRAPS_BINDIR="${INSTALL_DIR}/centreon/bin"
+SNMPTT_BINDIR="${INSTALL_DIR}/centreon/bin"
+CENTCORE_INSTALL_INIT=1
+CENTCORE_INSTALL_RUNLVL=1
+CENTSTORAGE_INSTALL_INIT=0
+CENTSTORAGE_INSTALL_RUNLVL=0
+CENTREONTRAPD_BINDIR="${INSTALL_DIR}/centreon/bin"
+CENTREONTRAPD_INSTALL_INIT=1
+CENTREONTRAPD_INSTALL_RUNLVL=1
+
+INSTALL_DIR_NAGIOS="${INSTALL_DIR}/centreon-engine"
+CENTREON_ENGINE_USER="${ENGINE_USER}"
+MONITORINGENGINE_USER="${CENTREON_USER}"
+MONITORINGENGINE_LOG="/var/log/centreon-engine"
+MONITORINGENGINE_INIT_SCRIPT="/etc/init.d/centengine"
+MONITORINGENGINE_BINARY="${INSTALL_DIR}/centreon-engine/bin/centengine"
+MONITORINGENGINE_ETC="${INSTALL_DIR}/centreon-engine/etc"
+NAGIOS_PLUGIN="${INSTALL_DIR}/centreon-plugins/libexec"
+FORCE_NAGIOS_USER=1
+NAGIOS_GROUP="${CENTREON_USER}"
+FORCE_NAGIOS_GROUP=1
+NDOMOD_BINARY="${INSTALL_DIR}/centreon-broker/bin/cbd"
+NDO2DB_BINARY="${INSTALL_DIR}/centreon-broker/bin/cbd"
+NAGIOS_INIT_SCRIPT="/etc/init.d/centengine"
+CENTREON_ENGINE_CONNECTORS="/usr/lib/centreon-connector"
+BROKER_USER="${BROKER_USER}"
+BROKER_ETC="${INSTALL_DIR}/centreon-broker/etc"
+BROKER_INIT_SCRIPT="/etc/init.d/cbd"
+BROKER_LOG="/var/log/centreon-broker"
+
+DIR_APACHE="/etc/apache2"
+DIR_APACHE_CONF="/etc/apache2/conf.d"
+APACHE_CONF="apache.conf"
+WEB_USER="www-data"
+WEB_GROUP="www-data"
+APACHE_RELOAD=1
+BIN_RRDTOOL="/usr/bin/rrdtool"
+BIN_MAIL="/usr/bin/mail"
+BIN_SSH="/usr/bin/ssh"
+BIN_SCP="/usr/bin/scp"
+PHP_BIN="/usr/bin/php"
+GREP="/bin/grep"
+CAT="/bin/cat"
+SED="/bin/sed"
+CHMOD="/bin/chmod"
+CHOWN="/bin/chown"
+
+RRD_PERL="/usr/lib/perl5"
+SUDO_FILE="/etc/sudoers"
+FORCE_SUDO_CONF=1
+INIT_D="/etc/init.d"
+CRON_D="/etc/cron.d"
+PEAR_PATH="/usr/share/php"
+EOF
+}
 
 echo "mysql-server-5.5 mysql-server/root_password password ${MYSQL_PASSWORD}
       mysql-server-5.5 mysql-server/root_password seen true
