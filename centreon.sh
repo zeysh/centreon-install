a93e7038e5c67635acbdd1d1e9d94792a4e209af
albundy83 <gbg@greencom-networks.com>
2015-12-22 11:37:08 +0100

    Update to latest widgets
    Latest widgets fix bug introduce with latest Centreon web 2.7 release

--- centreon.sh
+++ centreon.sh
@@ -27,11 +27,11 @@ CENTREON_URL="${BASE_URL}/centreon/centreon-web-${CENTREON_VER}.tar.gz"
 CLAPI_URL="${BASE_URL}/Modules/CLAPI/centreon-clapi-${CLAPI_VER}.tar.gz"
 NAGVIS_MOD_URL="${BASE_URL}/Modules/centreon-nagvis/centreon-nagvis-${NAGVIS_MOD_VER}.tar.gz"
 ## Sources widgets
-WIDGET_HOST_VER='1.3.2'
-WIDGET_HOSTGROUP_VER='1.2.1'
-WIDGET_SERVICE_VER='1.3.2'
-WIDGET_SERVICEGROUP_VER='1.2.1'
-WIDGET_GRAPH_VER='1.2.0'
+WIDGET_HOST_VER='1.4.0'
+WIDGET_HOSTGROUP_VER='1.3.0'
+WIDGET_SERVICE_VER='1.4.0'
+WIDGET_SERVICEGROUP_VER='1.3.0'
+WIDGET_GRAPH_VER='1.3.0'
 WIDGET_BASE='https://s3-eu-west-1.amazonaws.com/centreon-download/public/centreon-widgets'
 WIDGET_HOST="${WIDGET_BASE}/centreon-widget-host-monitoring/centreon-widget-host-monitoring-${WIDGET_HOST_VER}.tar.gz"
 WIDGET_HOSTGROUP="${WIDGET_BASE}/centreon-widget-hostgroup-monitoring/centreon-widget-hostgroup-monitoring-${WIDGET_HOSTGROUP_VER}.tar.gz"
@@ -560,10 +560,6 @@ chmod 775 /var/lib/centreon-broker/
 ## drwxr-xr-x 3 root root 15 Feb  4 20:31 centreon-engine
 chown ${ENGINE_USER}:${ENGINE_GROUP} /var/lib/centreon-engine/
 
-# Workaround for error with Graph Monitoring widget
-# PHP Fatal error: Call to undefined method DB_Error::fetchRow() in /usr/local/centreon/www/class/centreonGMT.class.php on line 298
-/bin/sed -i '292i\        if (!isset($sid)) {\n            return 0;\n        }\n' ${INSTALL_DIR}/centreon/www/class/centreonGMT.class.php
-
 }
 
 ##ADDONS
@@ -598,13 +594,11 @@ echo '
 '
 cd ${DL_DIR}
   wget -qO- ${WIDGET_HOST} | tar -C ${INSTALL_DIR}/centreon/www/widgets --strip-components 1 -xzv
-  mkdir ${INSTALL_DIR}/centreon/www/widgets/hostgroup-monitoring
-  wget -qO- ${WIDGET_HOSTGROUP} | tar -C ${INSTALL_DIR}/centreon/www/widgets/hostgroup-monitoring --strip-components 2 -xzv
+  wget -qO- ${WIDGET_HOSTGROUP} | tar -C ${INSTALL_DIR}/centreon/www/widgets --strip-components 1 -xzv
   wget -qO- ${WIDGET_SERVICE} | tar -C ${INSTALL_DIR}/centreon/www/widgets --strip-components 1 -xzv
-  mkdir ${INSTALL_DIR}/centreon/www/widgets/servicegroup-monitoring
-  wget -qO- ${WIDGET_SERVICEGROUP} | tar -C ${INSTALL_DIR}/centreon/www/widgets/servicegroup-monitoring --strip-components 2 -xzv
-  chown -R ${CENTREON_USER}:${CENTREON_GROUP} ${INSTALL_DIR}/centreon/www/widgets
+  wget -qO- ${WIDGET_SERVICEGROUP} | tar -C ${INSTALL_DIR}/centreon/www/widgets --strip-components 1 -xzv
   wget -qO- ${WIDGET_GRAPH} | tar -C ${INSTALL_DIR}/centreon/www/widgets --strip-components 1 -xzv
+  
   wget -qO- ${NAGVIS_MOD_URL} | tar -C ${INSTALL_DIR}/centreon/www/modules centreon-nagvis-${NAGVIS_MOD_VER}/www --strip-components 3 -xzv
   # Added to fix a bug in nagvis module
   cat >> ${INSTALL_DIR}/centreon/www/modules/centreon-nagvis/sql/install.sql << 'EOF'
